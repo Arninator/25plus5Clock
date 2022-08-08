7ef23f3479e11350be62e47fcb7fea4a556ac6d6
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-08-08 12:21:03 +0000

    do the layout

--- assets/javaScript.js
+++ assets/javaScript.js
@@ -5,23 +5,27 @@ class Clock25 extends React.Component {
     render () {
         return(
             <div>
-                <div id="session-label" class="controls">
-                    <button id="session-decrement">&darr;</button>
-                    <div id="session-length">25</div>
-                    <button id="session-increment">&uarr;</button>
-                </div>
-                <div id="break-label" class="controls">
-                    <button id="break-decrement">&darr;</button>
-                    <div id="break-length">5</div>
-                    <button id="break-increment">&uarr;</button>
-                </div>
-                <div id="timer-label">
-                    <div>Session</div>
-                    <div id="time-left">24:59</div>
-                    <div id="controls">
-                        <div id="start_stop"></div>
-                        <div id="reset"></div>
+                <div class="controls">
+                    <div id="session-label" class="controls">LENGTH: 
+                        <button id="session-decrement">&darr;</button>
+                        <div id="session-length">25</div>
+                        <button id="session-increment">&uarr;</button>
+                        min
                     </div>
+                    <div id="break-label" class="controls">BREAK: 
+                        <button id="break-decrement">&darr;</button>
+                        <div id="break-length">5</div>
+                        <button id="break-increment">&uarr;</button>
+                        min
+                    </div>
+                </div>
+                
+                <div id="timer-label">
+                    <div class="controls">
+                        <div id="start_stop"><i class="fa fa-pause"></i></div>
+                        <div id="reset"><i class="fa fa-stop"></i></div>
+                    </div>
+                    <div id="time-left">24:59</div>
                 </div>
             </div>
         )
